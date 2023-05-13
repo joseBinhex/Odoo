8bbfc4c50b201504117a83cea69c9622d036243f
joseBinhex <128590394+joseBinhex@users.noreply.github.com>
2023-05-13 12:20:59 +0100

    Delete openacademy directory

--- openacademy/models/gallery.py
+++
@@ -1,9 +0,0 @@
-from odoo import models, fields
-
-class ImageGallery(models.Model):
-    _name = 'openacademy.gallery'
-    _description = 'Image Gallery'
-    _rec_name = 'name'
-
-    name = fields.Char(string='Name', required=True)
-    image = fields.Binary(string='Image')
--- openacademy/models/openacademy_partner.py
+++
@@ -1,9 +0,0 @@
-
-from odoo import models, fields, api
-
-class partner(models.Model):
-    _inherit = 'res.partner'
-    _description = 'Esto es una descripcion!'
-
-    instructor = fields.Boolean(string='Instructor')
-    session_partner = fields.Many2many(comodel_name='openacademy.sessions')
